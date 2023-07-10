a1c2c17510cc2cd346987830284fee4fa6521223
Anthony <87497710+Tony182x@users.noreply.github.com>
2023-07-10 13:16:16 -0600

    Creacion del proyecto "Loves by Cakes" y Home de la aplicacion con sus hoja de estilos CSS

---
+++ OneDrive/Documents/NetBeansProjects/ProyectoAppWeb/src/test/java/com/Proyecto/app/LovesByCakesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Proyecto.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LovesByCakesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
