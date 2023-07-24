6c67b2dd2bd1288e666a4808554fb2c0e7939f9a
Ebrenes22 <e-j-bm@outlook.com>
2023-07-23 21:31:40 -0600

    Update de index.html

---
+++ OneDrive/Documents/NetBeansProjects/ProyectoAppWeb/src/main/java/com/Proyecto/app/controller/MainController.java
@@ -0,0 +1,14 @@
+package com.Proyecto.app.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index"; // Esto devolverá el nombre del archivo HTML sin la extensión
+    }
+}
