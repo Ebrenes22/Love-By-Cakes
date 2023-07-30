b03c63d771a69b98856aef825d67efca617642ca
Ebrenes22 <e-j-bm@outlook.com>
2023-07-29 18:56:41 -0600

    Reconfiguracion

--- OneDrive/Documents/NetBeansProjects/ProyectoAppWeb/src/main/java/com/Proyecto/domain/Producto.java
+++ OneDrive/Documents/NetBeansProjects/ProyectoAppWeb/src/main/java/com/Proyecto/domain/Producto.java
@@ -9,8 +9,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import java.io.Serializable;
 import lombok.Data;
@@ -37,9 +35,9 @@ public class Producto implements Serializable {
     private String rutaImagen;
     private boolean activo;
 
-    @ManyToOne
-    @JoinColumn(name = "id_categoria")
-    Producto producto;
+    //@ManyToOne
+    //@JoinColumn(name = "id_categoria")
+    //Producto producto;
 
     public Producto() {
     }
